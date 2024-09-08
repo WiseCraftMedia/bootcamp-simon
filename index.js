b34f9bdce0b808f2bfcef361731fc3d28b0ac45f
WiseCraftMedia <wisecraftmedia@gmail.com>
2024-09-08 12:17:40 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// $("h1").addClass("big-title");
+
+// $("body").keypress(ev => {
+//   console.log($("h1").text);
+//   if ($("h1").html() === "Go away") {
+//     $("h1").text(ev.key);
+//   } else {
+//     $("h1").html($("h1").html() + ev.key);
+//   }
+// });
