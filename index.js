d8bd6a44e46d7425bf3904c6458e8d9971946f4e
WiseCraftMedia <wisecraftmedia@gmail.com>
2024-09-08 14:05:08 -0400

    flashing and sounds

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+$("button").on("click", function () {
+  sequence();
+});
+
+var btnColors = ["red", "blue", "green", "yellow"];
+var pattern = [];
+
+function sequence() {
+  var rand = Math.floor(Math.random() * 4);
+  var chosen = btnColors[rand];
+  pattern.push(chosen);
+  $("#" + chosen)
+    .fadeOut(50)
+    .fadeIn(50)
+    .fadeOut(50)
+    .fadeIn(50);
+  var patternSound = new Audio(`./sounds/${chosen}.mp3`);
+  patternSound.play();
+  return pattern;
+}
+sequence();
+
 // $("h1").addClass("big-title");
 
 // $("body").keypress(ev => {
