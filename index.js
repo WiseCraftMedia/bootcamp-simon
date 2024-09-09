ca30c342c5337e6c16296e201839d889826fabe8
WiseCraftMedia <wisecraftmedia@gmail.com>
2024-09-09 14:01:13 -0400

    illogic

--- index.js
+++ index.js
@@ -1,24 +1,62 @@
-$("button").on("click", function () {
-  sequence();
+// Start Game
+$(document).on("keydown", function (ev) {
+  if (ev.key === "a" || ev.key === "A") {
+    $(document).off("keydown");
+    console.log(pattern.length);
+    sequence();
+  }
 });
+
+$(".btn").on("click", storeClick);
 
 var btnColors = ["red", "blue", "green", "yellow"];
 var pattern = [];
+var userPattern = [];
+var level = 0;
+
+function storeClick(ev) {
+  var userChosenColor = ev.currentTarget.id;
+  var colorIndex = btnColors.indexOf(userChosenColor);
+  userPattern.push(userChosenColor);
+  playSound(userChosenColor);
+  sequence(userChosenColor);
+  checkAnswer(colorIndex, userChosenColor);
+}
+
+function checkAnswer(latestClick, userChosenColor) {
+  // if latest answer = color Index, success
+  if (userPattern[latestClick] === pattern[latestClick]) {
+    console.log("success");
+  } else {
+    console.log("incorrect, was " + userChosenColor);
+  }
+  console.log(pattern.length);
+}
 
 function sequence() {
   var rand = Math.floor(Math.random() * 4);
-  var chosen = btnColors[rand];
-  pattern.push(chosen);
-  $("#" + chosen)
+  var rngChosen = btnColors[rand];
+  pattern.push(rngChosen);
+  playSound(rngChosen);
+  animatePress(rngChosen);
+  $("#level-title").text(`Level ${level}`);
+  level++;
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor)
     .fadeOut(50)
     .fadeIn(50)
     .fadeOut(50)
     .fadeIn(50);
-  var patternSound = new Audio(`./sounds/${chosen}.mp3`);
-  patternSound.play();
-  return pattern;
 }
-sequence();
+
+function playSound(name) {
+  var patternSound = new Audio(`./sounds/${name}.mp3`);
+  patternSound.play();
+}
+
+// clickHandler(colorButton);
 
 // $("h1").addClass("big-title");
 
